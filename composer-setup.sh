a334b7174823abb777fdeef0f50abd794fe27571
Jordalgo <jordalgo@spotify.com>
2015-12-21 13:04:54 -0500

    update composer script
    Change-Id: Ib19ec1fc5e1018bf6eff7eced106c70442dd7ad3

--- composer-setup.sh
+++ composer-setup.sh
@@ -6,6 +6,11 @@ PHP_IS_INSTALLED=$?
 
 [[ $PHP_IS_INSTALLED -ne 0 ]] && { printf "!!! PHP is not installed.\n    Installing Composer aborted!\n"; exit 0; }
 
+echo ">>> Copying build-config-example to build-config.json"
+cp build-config-example.json build-config.json
+echo ">>> Copying composer-example to composer.json"
+cp composer-example.json composer.json
+
 # Test if Composer is installed
 composer -v > /dev/null 2>&1
 COMPOSER_IS_INSTALLED=$?
@@ -13,14 +18,13 @@ COMPOSER_IS_INSTALLED=$?
 if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
   echo ">>> Installing Composer"
   curl -sS https://getcomposer.org/installer | php
+  echo ">>> Installing Wordpress and Plugins with Composer!"
+  php composer.phar install
+else 
+  echo ">>> Installing Wordpress and Plugins with Composer!"
+  composer install
 fi
 
-echo ">>> Copying build-config-example to build-config.json"
-cp build-config-example.json build-config.json
-echo ">>> Copying composer-example to composer.json"
-cp composer-example.json composer.json
-echo ">>> Installing Wordpress and Plugins with Composer!"
-php composer.phar install
 echo ">>>> Success!"
 
 exit 0;
